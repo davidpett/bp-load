c433a8a0dda4b096831d028a958aa0bee9c52816
David Pett <david.pett@digitaria.com>
2014-03-21 01:02:43 -0500

    adding some more styles and gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,43 @@
 'use strict';
 var gulp = require('gulp'),
+    connect = require('gulp-connect'),
     $ = require('gulp-load-plugins')();
 
 gulp.task('styles', function() {
-  return gulp.src('styles/**/*.scss')
+  return gulp.src('styles/sass/*.scss')
     .pipe($.rubySass({
       style: 'compressed'
     }))
+    .pipe($.autoprefixer('last 1 version'))
     .pipe(gulp.dest('styles'))
-    .pipe($.size());
 });
 
 gulp.task('scripts', function() {
-  return gulp.src('scripts/**/*.js')
+  return gulp.src(['scripts/**/*.js', '!scripts/**/*.min.js'])
     .pipe($.jshint('.jshintrc'))
     .pipe($.jshint.reporter('default'))
+    .pipe($.uglify())
+    .pipe($.concat('bpload.min.js'))
     .pipe(gulp.dest('scripts'))
-    .pipe($.size());
 });
 
 gulp.task('default', ['styles', 'scripts']);
+
+gulp.task('connect', connect.server({
+  root: [__dirname],
+  port: 9000,
+  livereload: true,
+  open: {}
+}));
+gulp.task('watch', ['connect'], function () {
+  gulp.watch([
+    '*.html',
+    'styles/**/*.css',
+    'app/scripts/**/*.js'
+  ], function (e) {
+    return gulp.src(e.path)
+      .pipe(connect.reload());
+  });
+  gulp.watch('styles/**/*.scss', ['styles']);
+  gulp.watch('scripts/**/*.js', ['scripts']);
+});
