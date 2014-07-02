e6ad5dfe564d5421ccea397347c6c750cc8018e8
David Pett <david.pett@digitaria.com>
2014-07-02 10:58:15 -0500

    added a .bpcontent class to the hmtl element to prevent FOUC

--- scripts/bpload.js
+++ scripts/bpload.js
@@ -4,9 +4,10 @@
     var queries = document.querySelectorAll('.bpload'),
         len = queries.length,
         obj = null,
-        media = null;
+        media = null,
+        content = document.querySelector('.bpcontent');
     while (len--) {
-      obj = queries[len],
+      obj = queries[len];
       media = obj.getAttribute('data-media');
       if (media && window.matchMedia(media).matches) {
         obj.setAttribute('media', media);
@@ -14,18 +15,26 @@
         obj.classList.remove('bpload');
       }
     }
+    if (content) {
+      content.classList.remove('bpcontent');
+    }
     if (!queries.length) {
       window.removeEventListener('resize', bpload);
     }
     queries = null;
     len = null;
     obj = null;
+    media = null;
+    content = null;
   }
-  if (typeof window.matchMedia === 'undefined') {
-    var obj = document.querySelectorAll('.bpload.bpall')[0];
+  if (typeof window.matchMedia !== 'undefined') {
+    var obj = document.querySelectorAll('.bpload.bpall')[0],
+        content = document.querySelector('.bpcontent');
     obj.setAttribute('media', obj.getAttribute('data-media'));
     obj.setAttribute('href', obj.getAttribute('data-href'));
+    content.className = content.className.replace(/(?:^|\s)bpcontent(?!\S)/,'');
     obj = null;
+    content = null;
   } else {
     window.addEventListener('resize', bpload);
     bpload();
